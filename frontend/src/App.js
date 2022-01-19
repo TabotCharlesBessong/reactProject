537c5ed80bba5472f99f641afd7294a44ce9c2c8
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-19 07:15:56 +0100

    innialiased a  new repo

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Chat from './components/Chat';
+import Join from './components/Join';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+}
+
+export default App;
