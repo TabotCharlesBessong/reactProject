98b7b5346cb1885bc3f7c10071da759529663346
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-20 21:43:44 +0100

    added a gitignore to my project

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-import Chat from './components/Chat';
-import Join from './components/Join';
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
-    </Router>
+     <>
+       <h1>Hello </h1>
+     </>    
   );
 }
 
