60f938f2011050a8c3c3426cce3d3bd9e22777bd
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-29 10:15:55 +0100

    created a redux store

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,9 +1,37 @@
 
 import React from 'react';
+import millfy from 'millify'
+import { Col, Row, Statistic, Typography } from 'antd';
+
+const {Title} = Typography
 
 const Homepage = () => {
   return (
-    <h1>Homepage</h1>
+    <>
+      <Title level={2} className='heading' >
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Crypto Currencies" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24 hour volume" value='5'/>
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value='5'/>
+        </Col>
+        {/* <Col span={12}> */}
+          {/* <Statistic title="Total Crypto Currencies" value='5'/> */}
+        {/* </Col> */}
+      </Row>
+    </>
   );
 };
 
---
+++ frontend/src/services/CryptoApi.js
@@ -0,0 +1,21 @@
+
+import {createApi , fetchBaseQuery} from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'SIGN-UP-FOR-KEY'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+
+const createRequest = (url)=>({url, headers:cryptoApiHeaders})
+
+export const CryptoApi = createApi({
+  reducerPath: 'CryptoAPi',
+  baseQuery:fetchBaseQuery({baseUrl}),
+  endpoints: (builder)=> ({
+    getCryptos:builder.query({
+      query: ()=> createRequest('/exchanges')
+    })
+  })
+})
