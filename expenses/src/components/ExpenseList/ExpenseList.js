c723b6d8ab9456c69339284e8ed649de3d445b48
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-09 14:04:32 +0100

    worked on some reducer functionality

--- expenses/src/components/ExpenseList/ExpenseList.js
+++ expenses/src/components/ExpenseList/ExpenseList.js
@@ -1,10 +1,26 @@
 
 import React from 'react';
+import {Cancel} from '@material-ui/icons'
+import Card from './Card';
 
 const ExpenseList = () => {
+  const list = [
+    {
+    title:'Made purchase',
+    price:23,
+    logo:`<Cancel/>`,
+    createdAt:Date.now(),
+    }
+  ]
   return (
     <div className="">
-      this is my Expense
+      {list.length ? 
+        list.map((item) => 
+          <Card item={item} />
+        )
+      :
+      null
+      }
     </div>
   );
 };
