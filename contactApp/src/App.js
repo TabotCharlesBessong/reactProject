9a2a3ea47d99693107822de8f20c7ce49b275bdf
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-11 08:00:48 +0100

    starting a new redux project

---
+++ contactApp/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import {ToastContainer} from "react-toastify"
+
+function App() {
+  return (
+    <div className="App">
+    <ToastContainer/>
+    
+    <h1>Welcome to react redux contact app</h1>
+    </div>
+  );
+}
+
+export default App;
