a5d6394a024587542ea01cfdc79173d9846ea674
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-07 09:29:11 +0100

    created the videos components

--- ytClone/src/components/Video/Video.js
+++ ytClone/src/components/Video/Video.js
@@ -1,10 +1,31 @@
 
 import React from 'react'
 import './_video.scss'
+import {AiFillEye} from 'react-icons/ai'
+import agent1  from '../../images/agents3.jfif'
+import flat2  from '../../images/family3.jfif'
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className="video">
+      <div className="video__top">
+        <img src={agent1} alt="" />
+        <span className='video__duration' > 09:32 </span>
+      </div>
+      <div className="video__title">
+        <h1>Create react app under 10 minutes</h1>
+      </div>
+      <div className="video__details">
+        <span>
+          <AiFillEye/> 4m views...
+        </span>
+        <span>5 days ago</span>
+      </div>
+      <div className="video__channel">
+        <img src={flat2} alt="" />
+        Avulion
+      </div>
+    </div>
   )
 }
 
