05613dd9db82f702e0a637b4e9894f91c5038b64
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-07 09:53:07 +0100

    adding routers

--- ytClone/src/App.js
+++ ytClone/src/App.js
@@ -6,23 +6,56 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Homescreen from './pages/HomeScreen/Homescreen';
 import Login from './pages/Login/Login';
 import './_app.scss'
+import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
-function App() {
+
+const Layout = ({children})=>{
   const [sidebar,setSidebar] = useState(false)
 
   const handleToggle = ()=>{
     setSidebar(!sidebar)
   }
+   return (
+     <>
+    <Header handleToggle={handleToggle} />
+    <div className="app__container border border-info ">
+      <Sidebar handleToggle={handleToggle} setSidebar={setSidebar} sidebar={sidebar}  />
+      <Container fluid className='app__main border border-warning ' >
+      {children}
+      </Container>
+    </div>
+    </>
+   )
+}
+
+function App() {
+  
   return (
     <>
-      {/* <Header handleToggle={handleToggle} />
-      <div className="app__container border border-info ">
-        <Sidebar handleToggle={handleToggle} setSidebar={setSidebar} sidebar={sidebar}  />
-        <Container fluid className='app__main border border-warning ' >
-          <Homescreen/>
-        </Container>
-      </div> */}
-      <Login/>
+      <Router>
+      <Switch>
+        <Route path='/' exact >
+          <Layout>
+            <Homescreen/>
+          </Layout>
+        </Route>
+        <Route path='/auth'>
+          <Login/>
+        
+        </Route>
+        <Route path='/search'  >
+          <Layout>
+            {/* <Homescreen/> */}
+            <h1>Search Result </h1>
+          </Layout>
+        </Route>
+        </Switch>
+
+        <Route>
+          <Redirect to='/' />
+        </Route>
+      </Router>
     </>
   );
 }
