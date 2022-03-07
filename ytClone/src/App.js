35f145b1f1b66f4df52385a5830da63b7d1f0ec1
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-07 09:40:16 +0100

    Login Screen

--- ytClone/src/App.js
+++ ytClone/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Homescreen from './pages/HomeScreen/Homescreen';
+import Login from './pages/Login/Login';
 import './_app.scss'
 
 function App() {
@@ -14,13 +15,14 @@ function App() {
   }
   return (
     <>
-      <Header handleToggle={handleToggle} />
+      {/* <Header handleToggle={handleToggle} />
       <div className="app__container border border-info ">
         <Sidebar handleToggle={handleToggle} setSidebar={setSidebar} sidebar={sidebar}  />
         <Container fluid className='app__main border border-warning ' >
           <Homescreen/>
         </Container>
-      </div>
+      </div> */}
+      <Login/>
     </>
   );
 }
