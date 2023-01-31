facb9abd875261b6350f8ad53abf05690603547c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-31 15:55:57 +0100

    added configuration and personal styling to tailwind css

--- instagramClone/tailwind.config.cjs
+++ instagramClone/tailwind.config.cjs
@@ -3,6 +3,24 @@ module.exports = {
 	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
+		colors:{
+			white:'#fff',
+			blue:{
+				medium:'#005c9a'
+			},
+			black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+		}
 	},
 	plugins: [],
 };
