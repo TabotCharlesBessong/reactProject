b7c61bdac396cda59af3902523aa722ba9c0477f
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-01-31 08:28:35 +0100

    removing css and adding background

--- instagramClone/src/App.jsx
+++ instagramClone/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
-import './index.css'
+// import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
