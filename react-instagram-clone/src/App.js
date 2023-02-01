a259567e5a97bf6ddd47de440b2b38af6d09b145
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-01 13:40:03 +0100

    clean

--- react-instagram-clone/src/App.js
+++ react-instagram-clone/src/App.js
@@ -1,30 +1,7 @@
-// import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import AddExpense from './pages/AddExpense/AddExpense';
-// import Home from './pages/Home';
+import React from 'react' 
 
-function App() {
-  return (
-    <div className="">
-      <Router>
-        <Header/>
-        <Switch>
-        <Route exact path='/' >
-          <Home/>
-        </Route>
-        <Route  path='/add' >
-          <AddExpense/>
-        </Route>
-        </Switch>
-        <div className="">Footer</div>
-      </Router>
-
-    </div>
-  );
+const App = () => {
+  return(
+    <div>Hello App</div>
+  )
 }
-
-export default App;
--- react-instagram-clone/src/redux/actionTypes/Expenses.js
+++
@@ -1,3 +0,0 @@
-
-export const ADD_EXPENSE = 'ADD_EXPENSE'
-export const DELETE_EXPENSE = 'DELETE_EXPENSE'
