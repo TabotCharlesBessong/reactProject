bb8055c99d13a3c095418b1ff98b9898f3a3117c
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-01 06:23:07 +0100

    404 page

--- instagramClone/src/App.jsx
+++ instagramClone/src/App.jsx
@@ -5,20 +5,22 @@ import * as ROUTES from './constants/routes'
 const Login = lazy(() => import ('./pages/Login'))
 const Signup = lazy(() => import ('./pages/Signup'))
 const Home = lazy(() => import ('./pages/Home'))
+const NotFound = lazy(() => import("./pages/NotFound"));
 // import './index.css'
 
 const  App = () => {
   return (
-    <Router>
-      <h2 className='text-4xl text-blue-500 text-center' >Hello the world</h2>
-      <Suspense fallback={<p>Loading...</p>} />
-      <Switch>
-        <Route path={ROUTES.LOGIN} component={Login} />
-        <Route path={ROUTES.SIGN_UP} component={Signup} />
-        <Route exact path={ROUTES.DASHBOARD} component={Home} />
-      </Switch>
-    </Router>
-  )
+		<Router>
+			<h2 className="text-4xl text-blue-500 text-center">Hello the world</h2>
+			<Suspense fallback={<p>Loading...</p>} />
+			<Switch>
+				<Route path={ROUTES.LOGIN} component={Login} />
+				<Route path={ROUTES.SIGN_UP} component={Signup} />
+				<Route exact path={ROUTES.DASHBOARD} component={Home} />
+				<Route exact path='*' component={NotFound} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App
