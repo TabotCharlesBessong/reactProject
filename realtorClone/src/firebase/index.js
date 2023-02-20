46866c00860b979ce0075452f39ddf8f276a1371
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-20 08:13:17 +0100

    hidding vulnerable credentials

--- realtorClone/src/firebase/index.js
+++ realtorClone/src/firebase/index.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDgl-5QUcBC9Tcu74nVys1b69jfAbzhdYw",
-	authDomain: "twitter-react-3371b.firebaseapp.com",
-	projectId: "twitter-react-3371b",
-	storageBucket: "twitter-react-3371b.appspot.com",
-	messagingSenderId: "532715213869",
-	appId: "1:532715213869:web:74897022a02fe32519d4a4",
-	measurementId: "G-S201F39EYY",
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+	appId: import.meta.env.VITE_APP_ID,
+	measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
