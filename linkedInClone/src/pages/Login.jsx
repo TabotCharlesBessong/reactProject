1233ea84f879f04b1ea705a7118d1cebb274e233
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-16 08:18:07 +0100

    feat: (protected route)

--- linkedInClone/src/pages/Login.jsx
+++ linkedInClone/src/pages/Login.jsx
@@ -1,23 +1,29 @@
 
 import React from "react";
-import { LoginComponent } from "../components";
+import { Loader, LoginComponent } from "../components";
 import { useEffect } from "react";
 import { auth } from "../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 const Login = () => {
+  const [laoding, setLaoding] = useState(true)
   const navigate = useNavigate()
   useEffect(() => {
     onAuthStateChanged(auth,(res) => {
       if(res?.accessToken){
         navigate('/home')
+      }else{
+        setLaoding(false)
       }
     })
   },[])
   return <div>
-    <LoginComponent/>
+    {
+      laoding ? <Loader/> : <LoginComponent/>
+    }
   </div>;
 };
 
