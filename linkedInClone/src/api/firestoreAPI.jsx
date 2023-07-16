2b8702edfad5ae194d8bdcbe111d22f36b11cdac
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-16 14:42:47 +0100

    bug: (unable to post to firestore)

---
+++ linkedInClone/src/api/firestoreAPI.jsx
@@ -0,0 +1,19 @@
+
+import { firestore } from "../firebaseConfig";
+import {addDoc,collection,doc} from 'firebase/firestore'
+import { toast } from "react-toastify";
+
+const postsRef = collection(firestore, "posts");
+const userRef = collection(firestore, "users");
+
+
+export const postStatus = (object) => {
+  addDoc(postsRef, object)
+    .then(() => {
+      console.log('Posted')
+      toast.success("Post has been added successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
