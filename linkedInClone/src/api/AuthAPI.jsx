7d26e8ed2d9861c0592f11a9ad9f8cf3e82aa0d7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-02-28 06:06:05 +0100

    adjusting the position of the logo

--- linkedInClone/src/api/AuthAPI.jsx
+++ linkedInClone/src/api/AuthAPI.jsx
@@ -1,28 +1,31 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
-import {GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
-import { auth } from '../firebaseConfig'
-
-
-export const LoginAPI = (email,password) => {
+export const LoginAPI = (email, password) => {
   try {
-    const response = signInWithEmailAndPassword(auth,email,password)    
-    return response
+    const response = signInWithEmailAndPassword(auth, email, password);
+    return response;
   } catch (error) {
     // return error
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
 
-
-export const RegisterAPI = (email,password) => {
+export const RegisterAPI = (email, password) => {
   try {
-    const response = createUserWithEmailAndPassword(auth,email,password)    
-    return response
+    const response = createUserWithEmailAndPassword(auth, email, password);
+    return response;
   } catch (error) {
     // return error
-    console.log(error.message) 
+    console.log(error.message);
   }
-}
+};
 
 export const GoogleSignInAPI = () => {
   try {
