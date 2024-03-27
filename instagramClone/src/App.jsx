40b38955c58f5ad843911a5dd3855d43e1d640e0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-27 05:25:19 +0100

    signup component and also changed favicon

--- instagramClone/src/App.jsx
+++ instagramClone/src/App.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import { PageLayout } from './component'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import React from "react";
+import { PageLayout, Signup } from "./component";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <PageLayout>
         <Routes>
-          <Route
-            path="/"
-            element={
-              "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, laboriosam ipsum pariatur maxime hic beatae natus ea rerum nesciunt dolores sint quod blanditiis libero eius esse voluptates repellendus aspernatur in!"
-            }
-          />
+          <Route path="/" element={<Signup />} />
         </Routes>
       </PageLayout>
     </Router>
   );
-}
+};
 
-export default App
+export default App;
--- instagramClone/src/component/index.js
+++ instagramClone/src/component/index.js
@@ -1,2 +1,4 @@
-export { default as PageLayoutSpinner } from "./common/PageLayoutSpinner";
 export { default as PageLayout } from "../layout/PageLayout";
+export { default as Signup } from "./auth/Signup";
+export { default as PageLayoutSpinner } from "./common/PageLayoutSpinner";
+
