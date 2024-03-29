bc10974ad52717ad2f32cd391f122811424d15fd
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-03-29 11:25:12 +0100

    user hooks

---
+++ instagramClone/src/hooks/user/useGetSuggestedUsers.js
@@ -0,0 +1,53 @@
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import useShowToast from "../utility/useShowToast";
+import { firestore } from "../../firebaseConfig";
+import useAuthStore from "../../store/authStore";
+
+const useGetSuggestedUsers = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [suggestedUsers, setSuggestedUsers] = useState([]);
+  const authUser = useAuthStore((state) => state.user);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getSuggestedUsers = async () => {
+      setIsLoading(true);
+      try {
+        const usersRef = collection(firestore, "users");
+        const q = query(
+          usersRef,
+          where("uid", "not-in", [authUser.uid, ...authUser.following]),
+          orderBy("uid"),
+          limit(3)
+        );
+
+        const querySnapshot = await getDocs(q);
+        const users = [];
+
+        querySnapshot.forEach((doc) => {
+          users.push({ ...doc.data(), id: doc.id });
+        });
+
+        setSuggestedUsers(users);
+        setIsLoading(false)
+      } catch (error) {
+        showToast("Error", error.message, "error");
+        setIsLoading(false);
+      }
+    };
+
+    if (authUser) getSuggestedUsers();
+  }, [authUser, showToast]);
+
+  return { isLoading, suggestedUsers };
+};
+
+export default useGetSuggestedUsers;
